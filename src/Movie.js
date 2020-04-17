1f373acd7e133985dfe3f7aa7b0bb107d6d91509
mhk0116 <mhk0116@gmail.com>
2020-04-18 02:22:18 +0900

    Bring the data from API

---
+++ src/Movie.js
@@ -0,0 +1,18 @@
+import React from   "react";
+import ProTypes, { checkPropTypes } from "prop-types";
+
+function Movie({id, year, title, summary, poster}){
+    return <h4>
+        {title}
+    </h4>
+}
+
+Movie.ProTypes ={
+    id: ProTypes.number.isRequired,
+    year: ProTypes.number.isRequired,
+    title: ProTypes.string.isRequired,
+    summary: ProTypes.string.isRequired,
+    poster: ProTypes.string.isRequired
+};
+
+export default Movie;
