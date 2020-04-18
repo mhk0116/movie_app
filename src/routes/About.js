9380b56362fcd1ee625fa092c206df0d21371aec
Minhwan Kim <mhk0116@gmail.com>
2020-04-18 22:55:01 +0900

    Page hosting

---
+++ src/routes/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+  return (
+    <div className="about__container">
+      <span>
+        “Finally, my first React practice is done.”
+      </span>
+      <span> well done </span>
+    </div>
+  )
+}
+
+export default About;
